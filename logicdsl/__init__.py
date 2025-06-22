dce7957dd038817db858645c3a13d75efee94649
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2025-06-21 20:05:11 -0400

    Add when-then helper

--- logicdsl/__init__.py
+++ logicdsl/__init__.py
@@ -22,6 +22,26 @@ def named(expr: BoolExpr, name: str) -> BoolExpr:
         """Return a BoolExpr with the provided name."""
         return expr.named(name)
 
+
+class _ImplicationBuilder:
+        """Helper object returned by :func:`when` to chain ``.then``."""
+
+        def __init__(self, cond: BoolExpr) -> None:
+                self._cond = cond
+
+        def then(self, expr: BoolExpr | Var) -> BoolExpr:
+                """Return implication ``cond >> expr``."""
+                return self._cond >> BoolExpr._B(expr)
+
+
+def when(cond: BoolExpr | Var) -> _ImplicationBuilder:
+        """Convenience builder for implication expressions.
+
+        ``when(p).then(q)`` is equivalent to ``p >> q`` but can be useful when
+        constructing conditions dynamically.
+        """
+        return _ImplicationBuilder(BoolExpr._B(cond))
+
 from .solver import LogicSolver, Soft
 
 __all__ = [
@@ -30,6 +50,7 @@ __all__ = [
 	"Expr",
         "BoolExpr",
         "named",
+        "when",
         # constraints
 	"distinct",
         "at_least_one",
